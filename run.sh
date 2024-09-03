fa156138a2d34325fb796fe5f1dbe05b9f25e7f3
Antoni Sawicki <as@tenoware.com>
2024-09-02 22:32:44 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 docker rm -f vncfox
 
-docker run --name vncfox -e WIDTH=1280 -e HEIGHT=1024 -v vncfox:/home/vncfox/.mozilla -v /Volumes/Tmp:/home/vncfox/Downloads -d -p 5900:5900 tenox7/vncfox:latest
+docker run -d \
+  --name vncfox \
+  -v vncfox:/home/vncfox/.mozilla \
+  -v /Volumes/Tmp:/home/vncfox/Downloads \
+  -p 5900:5900 \
+  -e WIDTH=1280 -e HEIGHT=1024 \
+  tenox7/vncfox:latest
